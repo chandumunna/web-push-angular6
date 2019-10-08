5dfbeae4d5a6e63ea39a3a850d8dfbe01db1b9cc
Azharul Islam <azharul.sylhet@gmail.com>
2019-10-08 16:55:08 +0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,15 @@
+// Replace with your credientials
+
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBcOU9XvdBpl6tllKAF7yNgW0qAwLkpgcM",
-  	authDomain: "push-notification-92f4b.firebaseapp.com",
-  	databaseURL: "https://push-notification-92f4b.firebaseio.com",
-  	projectId: "push-notification-92f4b",
+    apiKey: "",
+  	authDomain: "",
+  	databaseURL: "",
+  	projectId: "",
   	storageBucket: "",
-  	messagingSenderId: "1050458095934",
-  	appId: "1:1050458095934:web:2f0e571a5ff5a1ba226c6f",
-  	measurementId: "G-E3W6J4C2ET"
+  	messagingSenderId: "",
+  	appId: "",
+  	measurementId: ""
   }
 };
