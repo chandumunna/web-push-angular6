a5938bd00e6e714cac404bcf8394657141c17ddf
Azharul <azharul.sylhet@gmail.com>
2019-10-08 16:47:16 +0600

    version01

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBcOU9XvdBpl6tllKAF7yNgW0qAwLkpgcM",
+  	authDomain: "push-notification-92f4b.firebaseapp.com",
+  	databaseURL: "https://push-notification-92f4b.firebaseio.com",
+  	projectId: "push-notification-92f4b",
+  	storageBucket: "",
+  	messagingSenderId: "1050458095934",
+  	appId: "1:1050458095934:web:2f0e571a5ff5a1ba226c6f",
+  	measurementId: "G-E3W6J4C2ET"
+  }
+};
